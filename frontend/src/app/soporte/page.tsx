0668587c15400e3bf955776133ed8a64ac6bcc31
Reyser <rzapata@unsa.edu.pe>
2024-08-20 06:05:58 -0500

    add frecuencies questions on soporte page

--- frontend/src/app/soporte/page.tsx
+++ frontend/src/app/soporte/page.tsx
@@ -1,7 +1,9 @@
+import { FAQ } from '@/components/pages/soporte/FAQ';
+
 export default function SoportePage() {
   return (
     <div className="flex-1">
-      soporte
+      <FAQ />
     </div>
   );
 }
